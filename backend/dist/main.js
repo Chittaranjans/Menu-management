3533baf814c4a1ebdfed0391ffa51213bc3f895e
chittaranjans <chittaedu18@gmail.com>
2024-12-27 22:18:32 +0530

    fix: standardize quotes in CORS configuration

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -11,7 +11,7 @@ async function bootstrap() {
         forbidNonWhitelisted: true,
     }));
     app.enableCors({
-        origin: 'https://menu-management-ebon.vercel.app',
+        origin: "https://menu-management-ebon.vercel.app",
     });
     await app.listen(process.env.PORT || 3005);
 }
